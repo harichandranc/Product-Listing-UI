00ad0b869f86266cd8f3d6058bc659ae076dd86d
Harichandran C <84121835+harichandranc@users.noreply.github.com>
2026-03-30 14:04:24 +0530

    Create products.js

---
+++ src/data/products.js
@@ -0,0 +1,32 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 14",
+    price: 80000,
+    category: "Electronics",
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    id: 2,
+    name: "T-Shirt",
+    price: 500,
+    category: "Clothing",
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    id: 3,
+    name: "Laptop",
+    price: 60000,
+    category: "Electronics",
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    id: 4,
+    name: "Shoes",
+    price: 2000,
+    category: "Footwear",
+    image: "https://via.placeholder.com/150"
+  }
+];
+
+export default products;
